13dc5bf2adbfeafb73f2fbaf4ce3eae8812f5113
nasha-o1 <na.sha.personal@gmail.com>
2024-10-30 11:39:40 +0530

    added nvim and tmux

---
+++ nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,16 @@
+return {
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+      theme = 'hyper',
+      config = {
+        week_header = {
+          enable = true,
+        },
+        shortcut = {}
+      },
+    }
+  end,
+  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+}
